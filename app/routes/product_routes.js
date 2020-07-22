8330aa0864aa0a20b9578c10475b4e42088b8656
Alexis1618 <smith.alexis.gabriel@gmail.com>
2020-07-22 17:16:17 -0400

    commented out product create and destroy routes
    Co-authored-by: Areesh Zindani <azindani786@gmail.com>
    Co-authored-by: Dajana Aghiarian <dajana.stankovic94@gmail.com>
    Co-authored-by: Kerah Jones <kerahjones@gmail.com>

--- app/routes/product_routes.js
+++ app/routes/product_routes.js
@@ -1,22 +1,22 @@
 // Express docs: http://expressjs.com/en/api.html
 const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
-const passport = require('passport')
+// const passport = require('passport')
 
 // pull in Mongoose model for products
 const Product = require('../models/product')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
-const customErrors = require('../../lib/custom_errors')
+// const customErrors = require('../../lib/custom_errors')
 
 // we'll use this function to send 404 when non-existant document is requested
-const handle404 = customErrors.handle404
+// const handle404 = customErrors.handle404
 
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `req.user`
-const requireToken = passport.authenticate('bearer', { session: false })
+// const requireToken = passport.authenticate('bearer', { session: false })
 
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
@@ -37,33 +37,35 @@ router.get('/products', (req, res, next) => {
     .catch(next)
 })
 
-// CREATE
-// POST /products
-router.post('/products', requireToken, (req, res, next) => {
-  Product.create(req.body.product)
-    // respond to succesful `create` with status 201 and JSON of new "product"
-    .then(product => {
-      res.status(201).json({ product: product.toObject() })
-    })
-    // if an error occurs, pass it off to our error handler
-    // the error handler needs the error message and the `res` object so that it
-    // can send an error message back to the client
-    .catch(next)
-})
+// The post route is commented out so user cant create products
+// // CREATE
+// // POST /products
+// router.post('/products', requireToken, (req, res, next) => {
+//   Product.create(req.body.product)
+//     // respond to succesful `create` with status 201 and JSON of new "product"
+//     .then(product => {
+//       res.status(201).json({ product: product.toObject() })
+//     })
+//     // if an error occurs, pass it off to our error handler
+//     // the error handler needs the error message and the `res` object so that it
+//     // can send an error message back to the client
+//     .catch(next)
+// })
 
-// DESTROY
-// DELETE /products/5a7db6c74d55bc51bdf39793
-router.delete('/products/:id', requireToken, (req, res, next) => {
-  Product.findById(req.params.id)
-    .then(handle404)
-    .then(product => {
-      // delete the product if it exists
-      product.deleteOne()
-    })
-    // send back 204 and no content if the deletion succeeded
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
+// The post route is commented out so user cant delete products
+// // DESTROY
+// // DELETE /products/5a7db6c74d55bc51bdf39793
+// router.delete('/products/:id', requireToken, (req, res, next) => {
+//   Product.findById(req.params.id)
+//     .then(handle404)
+//     .then(product => {
+//       // delete the product if it exists
+//       product.deleteOne()
+//     })
+//     // send back 204 and no content if the deletion succeeded
+//     .then(() => res.sendStatus(204))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
 
 module.exports = router
